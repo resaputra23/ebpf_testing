0fe188ab7f3fd47c76dd8271be080fd241d44736
resaputra23 <resaputra23@gmail.com>
2025-04-17 09:11:06 +0000

    Updated interface laptop

--- main.go
+++ main.go
@@ -25,7 +25,8 @@ func main() {
     }
     defer objs.Close()
 
-    ifname := "eth0" // Change this to an interface on your machine.
+    ifname := "wlp3s0" // Change this to an interface on your machine.
+
     iface, err := net.InterfaceByName(ifname)
     if err != nil {
         log.Fatalf("Getting interface %s: %s", ifname, err)
